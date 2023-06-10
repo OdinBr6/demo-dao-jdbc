0e0ff3fee9063415de4cf42b05bdbc2554254d08
CristianRamos <crisramosrodrigues@hotmail.com>
2023-06-10 19:20:17 -0300

    Finished project

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,13 +41,13 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
-		System.out.println("\n=== TEST 5: seller insert ====");
+		System.out.println("\n=== TEST 5: seller update ====");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
 		
-		System.out.println("\n=== TEST 6: seller insert ====");
+		System.out.println("\n=== TEST 6: seller delete ====");
 		System.out.println("Enter id for delet test: ");
 		int id = sc.nextInt();
 		sellerDao.deleteById(id);
